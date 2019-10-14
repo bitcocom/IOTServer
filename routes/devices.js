180729c5064d94f0291baf63e9be2685fce761a0
2017-076 <2017-076@LAPTOP-SICIJHRH>
2019-10-14 22:41:54 +0900

    commit

--- routes/devices.js
+++ routes/devices.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoDB=require("mongodb").MongoClient;
+var url="mongodb://127.0.0.1:27017/iot";
+var dbObj=null;
+mongoDB.connect(url, function(err, db){
+	dbObj=db;
+	console.log("DB Connect.......");
+});
+
 var mqtt=require("mqtt"); 
 var client=mqtt.connect("mqtt://172.30.1.15");
 
@@ -17,5 +25,18 @@ router.post('/led/:flag', function(req, res, next) {
    }
 });
 
+// Android APP에서 추가된 부분
+router.get('/:device/:sensor', function(req, res, next){	
+	var sensorLogs=null;
+	if(req.params.sensor=="dht11"){
+		sensorLogs=dbObj.collection('dht11');
+	}else{
+		//sensorLogs=dbObj.collection('mq2');
+	}
+    sensorLogs.find({}).limit(10).sort({created_at:-1}).toArray(function(err, results){
+        if(err) res.send(JSON.stringify(err));		            	 
+        else res.send(JSON.stringify(results));
+   });		
+});
 module.exports = router;
 
